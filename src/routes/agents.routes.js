103f6ac10545e486e92e811526888d7c69dae5c5
HeyAryan <82263728+HeyAryan@users.noreply.github.com>
2025-09-17 22:41:56 +0530

    Update agents.routes.js

--- src/routes/agents.routes.js
+++ src/routes/agents.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { getAgents } = require('../controllers/agent.controller');
-
+const { auth } = require('../middleware/auth');
 const router = express.Router();
 
-router.get('/', getAgents);
+router.get('/', auth, getAgents);
 
 module.exports = router;
 
